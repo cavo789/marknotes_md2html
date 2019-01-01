27e407b224c4b114c80292356cc9d1072648c5f6
Christophe <cavo789@yahoo.com>
2019-01-01 18:20:16 +0100

    Abandonment of jQuery and migration to vue.js
    Except for clipboard.min.js which requires jQuery

--- index.php
+++ index.php
@@ -14,37 +14,13 @@ declare(strict_types = 1);
  *      in an email f.i.
  *
  * Conversion is made in javascript, no server side action needed.
+ *
+ * Last mod:
+ * 2018-12-31 - Abandonment of jQuery and migration to vue.js
+ *                  Except for clipboard.min.js which requires jQuery
  */
 
 define('REPO', 'https://github.com/cavo789/marknotes_md2html');
-
-// A demo text
-$md =
-    "---\n" .
-    "title: 'A title'\n" .
-    "---\n\n" .
-    "# Test\n\n" .
-    "> This is a demo text\n\n" .
-    "![banner](https://raw.githubusercontent.com/cavo789/marknotes_md2html/master/image/banner.jpg)\n\n" .
-    "Lorem ipsum dolor ==sit amet==, consectetur *adipiscing* ==elit==.\n" .
-    "[Demo site](https://www.avonture.be/marknotes_md2html/)\n\n" .
-    "```sql\n" .
-    "SELECT ... FROM ... WHERE ... ORDER BY ...\n" .
-    "```\n\n" .
-    "| Column 1 Header | Column 2 Header |\n" .
-    "| --- | --- |\n" .
-    "| Row 1-1 | Row 1-2 |\n" .
-    "| Row 2-1 | Row 2-2 |\n\n" .
-    "Demo of `html` source code:\n\n" .
-    "```html\n" .
-    "<div class=\"col-sm\">\n" .
-    "   <title>Marknotes - MD2HTML - Quick convert markdown to HTML</title>\n" .
-    "</div>\n" .
-    "```\n\n" .
-    "```php\n" .
-    "<?php\n" .
-    "   echo \$variable;\n" .
-    "```\n";
 
 // Get the GitHub corner
 $github = '';
@@ -113,6 +89,10 @@ if (is_file($cat = __DIR__ . DIRECTORY_SEPARATOR . 'octocat.tmpl')) {
                 width: 100%;
                 height: -20px;
             }
+            #HTML_CODE {
+                position: fixed;
+                top: -100px;
+            }
         </style>
     </head>
     <body>
@@ -120,136 +100,168 @@ if (is_file($cat = __DIR__ . DIRECTORY_SEPARATOR . 'octocat.tmpl')) {
         <header class="page-header">
             <h1>Marknotes - MD2HTML</h1>
         </header>
-        <main role="main" class="container-fluid">
-            <div class="container-fluid">
-                <details>
-                    <summary>How to use?</summary>
+        <div id="app">
+            <main role="main" class="container-fluid">
+                <div class="container-fluid">
+                    <how-to-use demo="https://raw.githubusercontent.com/cavo789/marknotes_md2html/master/image/demo.gif"></how-to-use>                
+                </div>
+                <div class="container-fluid">
                     <div class="row">
-                        <div class="col-sm">
-                            <ol>
-                                <li>Type the markdown code in the text area here below</li>
-                                <li>Click on the Convert button to see the HTML rendering 
-                                    at the right side</li>
-                                <li>If looks fine, click on the Copy to clipboard button 
-                                    so you can f.i. paste the rendering in an email</li>
-                            </ol>
-                            <h2>Tips</h2>
-                            <ul>
-                                <li>Would you like to draw attention to some of your content? 
-                                    Add an equal double before and after like in 
-                                    <pre>==Please read this==</pre></li>
-                            </ul>
+                        <div class="col-sm" v-if="showEditor">
+                            <textarea class="form-control" rows="20" v-model="Markdown"></textarea>
+                            <!-- Textarea required for the Copy into clipboard so we can get the HTML code-->
+                            <textarea id="HTML_CODE">{{ HTML }}</textarea>
                         </div>
                         <div class="col-sm">
-                            <img height="300px" src="https://raw.githubusercontent.com/cavo789/marknotes_md2html/master/image/demo.gif" alt="Demo">
+                            <article id="HTML" v-html="HTML" class="markdown-body"><article>
                         </div>
-                    </div>
-                </details>
-            </div>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-sm">
-                        <textarea class="form-control" rows="20" id="editor" name="editor"><?php echo $md;?></textarea>
-                    </div>
-                    <div class="col-sm">
-                        <article id="HTML" class="d-none markdown-body"><article>
                     </div>
                 </div>
-            </div>
-        </main>
-        <footer class="footer">
-            <div class="buttons container-fluid">
-                <button type="button" id="btnConvert" class="btn btn-primary">
-                    Convert
-                </button> 
-                <button class="btnToggle btn btn-success">
-                    Toggle editor
-                </button>
-                <button disabled="disabled" class="btnClipboard d-none btn btn-success" data-clipboard-target="#HTML">
-                    Copy to clipboard
-                </button>
-            </div>
-        </footer>
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous" type="text/javascript"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" type="text/javascript"></script>
+            </main>
+            <footer class="footer">
+                <div class="buttons container-fluid">
+                    <button v-if="HTML" title="Show/Hide the editor"  
+                        class="btnToggle btn btn-success" @click="doToggle">
+                        Toggle editor
+                    </button>
+                    <button v-if="HTML"  title="Copy the rendered HTML in the clipboard"  
+                        class="btnClipboard btn btn-success" data-clipboard-target="#HTML">
+                        Copy
+                    </button>
+                    <button v-if="HTML" title="Copy the HTML source code (with tags) in the clipboard" 
+                        class="btnClipboard btn btn-success" data-clipboard-target="#HTML_CODE">
+                        Copy source
+                    </button>
+                </div>
+            </footer>
+        </div>
+        <script src="https://unpkg.com/vue"></script>
         <script src="https://unpkg.com/marked@0.3.6" type="text/javascript"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.13.0/prism.min.js" type="text/javascript"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.13.0/components/prism-php.js" type="text/javascript"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js" type="text/javascript"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.13.0/prism.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.13.0/components/prism-php.js"></script>
+
+        <!-- Clipboard requires jQuery -->
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js"></script>
+
         <script type="text/javascript">
+
             $(document).ready(function() {
+                // jQuery part, handle the Copy in the clipboard feature
                 if (typeof ClipboardJS === 'function') {
-
                     $('.btnClipboard').removeAttr("disabled");
-
                     var clipboard = new ClipboardJS('.btnClipboard');
-                    
+
                     clipboard.on('success', function(e) {
                         alert('HTML rendering copied!');
                         e.clearSelection();
                     });
                 }
+            });
 
-                function convertMD2HTML() {
-
-                    var $markdown = $('#editor').val();
-
-                    // Replace the YAML block that can be at the top of the file, f.i.
-                    //      ---
-                    //      title: 'a title'
-                    //      date: 2018-12-25
-                    //      ---
-                    // Just remove it
-                    try {
-                        $markdown = $markdown.replace(/\s*^-{3}[.\S\s]*^-{3}/mg, "");    
-                    } catch (error) {
+            Vue.component('how-to-use', {
+                props: {
+                    demo: {
+                        type: String,
+                        required: true
                     }
+                },
+                template:
+                    `<details>
+                        <summary>How to use?</summary>
+                        <div class="row">
+                                <div class="col-sm">
+                                    <ol>
+                                        <li>Type the markdown code in the text area here below</li>
+                                        <li>Click on the Convert button to see the HTML rendering 
+                                            at the right side</li>
+                                        <li>If looks fine, click on the Copy to clipboard button 
+                                            so you can f.i. paste the rendering in an email</li>
+                                    </ol>
+                                    <h2>Tips</h2>
+                                    <ul>
+                                        <li>Would you like to draw attention to some of your content? 
+                                            Add an equal double before and after like in 
+                                            <pre>==Please read this==</pre></li>
+                                    </ul>
+                                </div>
+                                <div class="col-sm"><img v-bind:src="demo" alt="Demo"></div>
+                            </div>
+                        </div>
+                    </details>`
+            });
 
-                    var $HTML = marked($markdown, { 
-                        sanitize: true,
-                        gfm: true, 
-                        smartypants: true, 
-                        tables: true 
-                    });
-
-                    // Handle a few custom tags
-                    //   ==WORD==       will put this word in a highlighted span
-                    $HTML = $HTML.replace(/([^=]*)==([^=]*)==([^=]*)/g, 
-                        "$1<span class='highlight'>$2</span>$3");
-
-                    // Render the markdown table to HTML
-                    $("#HTML").html($HTML);
-
-                    // Add bootstrap classes
-                    $("#HTML table").addClass("table table-hover table-striped");
-
-                    if (typeof Prism === 'object') {
-                        // Use prism.js and highlight source code
-                        Prism.highlightAll();
+            var app = new Vue({
+                el: '#app',
+                data: {
+                    Markdown: "---\n" +
+                        "title: 'A title'\n" +
+                        "---\n\n" +
+                        "# Test\n\n" +
+                        "> This is a demo text\n\n" +
+                        "![banner](https://raw.githubusercontent.com/cavo789/marknotes_md2html/master/image/banner.jpg)\n\n" +
+                        "Lorem ipsum dolor ==sit amet==, consectetur *adipiscing* ==elit==.\n" +
+                        "[Demo site](https://www.avonture.be/marknotes_md2html/)\n\n" +
+                        "```sql\n" +
+                        "SELECT ... FROM ... WHERE ... ORDER BY ...\n" +
+                        "```\n\n" +
+                        "| Column 1 Header | Column 2 Header |\n" +
+                        "| --- | --- |\n" +
+                        "| Row 1-1 | Row 1-2 |\n" +
+                        "| Row 2-1 | Row 2-2 |\n\n" +
+                        "Demo of `html` source code:\n\n" +
+                        "```html\n" +
+                        "<div class=\"col-sm\">\n" +
+                        "   <title>Marknotes - MD2HTML - Quick convert markdown to HTML</title>\n" +
+                        "</div>\n" +
+                        "```\n\n" +
+                        "```php\n" +
+                        "<?php\n" +
+                        "   echo \$variable;\n" +
+                        "```\n",
+                    showEditor: true
+                },
+                methods: {
+                    doToggle(event) {
+                        this.showEditor = !(this.showEditor);
                     }
+                },
+                computed: {
+                    HTML() {
+                        if (this.Markdown == '') {
+                            return '';
+                        }
 
-                    // Finally show the copy clipboard button and the HTML result
-                    $('.btnClipboard').removeClass('d-none');
-                    $('#HTML').removeClass('d-none');
+                        var $markdown = this.Markdown;
 
-                    return;
+                        // Replace the YAML block that can be at the top of the file, f.i.
+                        //      ---
+                        //      title: 'a title'
+                        //      date: 2018-12-25
+                        //      ---
+                        // Just remove it
+                        try {
+                            $markdown = $markdown.replace(/\s*^-{3}[.\S\s]*^-{3}/mg, "");
+                            $markdown = $markdown.trim();
+                        } catch (error) {
+                        }
+
+                        var $HTML = marked($markdown, { 
+                            sanitize: true,
+                            gfm: true, 
+                            smartypants: true, 
+                            tables: true 
+                        });
+
+                        // Handle a few custom tags
+                        //   ==WORD==       will put this word in a highlighted span
+                        $HTML = $HTML.replace(/([^=]*)==([^=]*)==([^=]*)/g, 
+                            "$1<span class='highlight'>$2</span>$3");
+
+                        // Add bootstrap classes
+                        return $HTML.replace('<table>', '<table class="table table-hover table-striped">');
+                    }
                 }
-
-                $("#editor").on('change keyup paste', function(e) {                    
-                    e.stopImmediatePropagation();
-                    convertMD2HTML();
-                });
-                
-                $('#btnConvert').click(function(e)  {
-                    e.stopImmediatePropagation();
-                    convertMD2HTML();
-                });
-
-                $('.btnToggle').click(function(e)  {
-                    e.stopImmediatePropagation();
-                    $('#editor').parent().toggleClass('d-none');
-                });
-
             });
         </script>
     </body>
